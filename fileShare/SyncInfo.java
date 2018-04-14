fc5265358d236f78466e2fb6db73542577a60870
javawayne <34524962+javawayne@users.noreply.github.com>
2018-04-14 19:34:26 -0400

    Update SyncInfo.java
    there are only 4 vars need to be sync. isInterestedOnMe and MyInterested are synced in P2P part; completedLabel and bitField are synced in Host and P2P. ConcurrentHashMap (SharingRate, neighborsInfo) do not need sync.

--- fileShare/SyncInfo.java
+++ fileShare/SyncInfo.java
@@ -1,5 +1,53 @@
 package fileShare;
 
-public class SyncInfo {
+import java.util.BitSet;
 
-}
+public class SyncInfo {
+	
+	/* instance variables */
+  private BitSet completedLabel;
+	private BitSet bitfield;
+	private boolean[] isInteresetedOnMe;
+	private boolean[] myInterest;
+	
+	/** constructor */
+	public SyncInfo(BitSet completedLabel, BitSet bitfield, boolean[] isInteresetedOnMe, boolean[] myInterest) {
+		 this.completedLabel = completedLabel;
+		 this.bitfield = bitfield;
+		 this.isInteresetedOnMe = isInteresetedOnMe;
+		 this.myInterest = myInterest;
+	}
+	
+	public BitSet getCompletedLabel() {
+		return completedLabel;
+	}
+	
+	public BitSet getBitField () {
+		return bitfield;
+	}
+	
+	public boolean[] getIsInterestedOnMe () {
+		return isInteresetedOnMe;
+	}
+	
+	public boolean[] getMyInterested () {
+		return myInterest;
+	}
+	
+	// isInterestedOnMe P2P Type2 & 3
+	public void setInterestedOnMe (int peerID, boolean onMe) {
+		synchronized (isInteresetedOnMe) {
+            isInteresetedOnMe[peerID] = onMe;
+        }
+	}
+	
+	// myInterested P2P Type4 & 5
+	public void setMyInterest (int peerID, boolean MyInt) {
+		synchronized (myInterest) {
+            myInterest[peerID] = MyInt;
+        }
+	}
+	
+	//
+ }
+
