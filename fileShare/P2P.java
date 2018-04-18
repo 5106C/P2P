05e102a373b513b6981ea867358e940972f29fc2
protozzzxw <37680408+protozzzxw@users.noreply.github.com>
2018-04-18 16:20:19 -0400

    Add files via upload

--- fileShare/P2P.java
+++ fileShare/P2P.java
@@ -1,5 +1,6 @@
 package fileShare;
 
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 import configuration.*;
@@ -17,92 +18,165 @@ public class P2P extends Thread {
 	private boolean ischoked;
 	private ConcurrentHashMap<Integer, Integer> downloadRate;
 	private ConcurrentHashMap<Integer, Neighbor> neighborInfo;
+	private boolean handshakefirst;
 
-	public P2P(Common common, PeerInfo peerinfo, SyncInfo syncinfo, int hostID, int neighborIndex, ConcurrentHashMap<Integer, Integer> downloadRate,
-	ConcurrentHashMap<Integer, Neighbor> neighborInfo) {
+	public P2P(Common common, PeerInfo peerinfo, SyncInfo syncinfo, int hostID, int neighborIndex,
+			ConcurrentHashMap<Integer, Integer> downloadRate, ConcurrentHashMap<Integer, Neighbor> neighborInfo,
+			boolean handshakefirst) {
 		this.common = common;
 		this.peerinfo = peerinfo;
 		this.syncinfo = syncinfo;
 		this.hostID = hostID;
 		this.neighborIndex = neighborIndex;
-		neighborID=peerinfo.getPeerID(neighborIndex);
-		neighbor=neighborInfo.get(neighborIndex);
-		ischoked=true;
-		this.requested=requested;
-		running=true;
+		this.neighborInfo = neighborInfo;
+		this.downloadRate = downloadRate;
+		neighborID = peerinfo.getPeerID(neighborIndex);
+		neighbor = this.neighborInfo.get(neighborIndex);
+		this.handshakefirst = handshakefirst;
+		ischoked = true;
+		running = true;
 	}
 
-		
+	public void stopRunning() {
+		running = false;
+	}
+
 	public void run() {
-	    if(handshakefirst){
-	        creatnewhandshake(neighbor);
-            System.out.println(hostID +"send my handshake to "+neighborID );    //neighborID, peerID
-            writetolog(hostID+ "makes a connection to peer "+ neighborID);
+		if (handshakefirst) {
+			creatnewhandshake();
+			System.out.println(hostID + "send my handshake to " + neighborID); // neighborID, peerID
+		}
 
-        }
+		while (running) {
+			Object receivedMessage = null;
+			receivedMessage = neighbor.receive();
+			if (receivedMessage instanceof HandShake) {
+				HandShake hs=(HandShake) receivedMessage;
+				if (!handshakeCheck(hs)) {
+					break;
+				}
+				if (!handshakefirst) {
+					creatnewhandshake();
+					System.out.println(hostID + " send handshake to " + neighborID);
+				}
+				int type = 5;
+				byte[] payload = bits2byte(syncinfo.getBitfield());
+				int length = payload.length;
+				ActualMessage bitfield = new ActualMessage(length, type, payload);
+				sendMsg(bitfield);
+			}
 
-        while(running){
-	        Object messagereceived=null;
-	       try{
-	           messagereceived=neighbor.receive();
+			if (receivedMessage instanceof ActualMessage) {
+				ActualMessage msg = (ActualMessage) receivedMessage;
+				//choke
+				if (msg.getType() == 0) {
+					ischoked = true;
+					String log="to do";
+					writelog(log);
+				}
+				//unchoke
+				else if (msg.getType() == 1) {
+					ischoked = false;
+					if(!syncinfo.interest(neighborIndex)) break;
+					int pieceIndex = choosePiece();
+					byte[] payload = int2byte(pieceIndex);
+					int type=6;
+					int length=1+4;
+					ActualMessage request = new ActualMessage(length, type, payload);
+					sendMsg(request);
+					String log="to do";
+					writelog(log);
+				}
+				//interested
+				else if (msg.getType() == 2) {
+					syncinfo.updateInterested(neighborIndex, true);
+					String log="to do";
+					writelog(log);
+				}
+				//not interested
+				else if (msg.getType() == 3) {
+					syncinfo.updateInterested(neighborIndex, false);
+					String log="to do";
+					writelog(log);
+				}
+				//have
+				else if(msg.getType() == 4) {
+					int pieceIndex=byte2int(msg.getPayload());
+					neighbor.updateBitfield(pieceIndex);
+					if(neighbor.isComplete()) syncinfo.updateCompletedPeers(neighborIndex);
+					if(!syncinfo.haspiecie(pieceIndex)) {
+						ActualMessage interested=new ActualMessage(1,2,null);						
+						sendMsg(interested);
+					}
+				}
+				//bitfield
+				else if(msg.getType() == 5) {
+					BitSet neighborBF=byte2bits(msg.getPayload());
+					neighbor.setBitfield(neighborBF);
+					if(checkBitfield()) {
+						syncinfo.updateInterest(neighborIndex, true);
+						ActualMessage interested=new ActualMessage(1,2,null);						
+						sendMsg(interested);
+					}
+					else {
+						syncinfo.updateInterest(neighborIndex, false);
+						ActualMessage notInterested=new ActualMessage(1,3,null);						
+						sendMsg(notInterested);
+					}
+				}
+				
+				else if(msg.getType() == 6) {
+					
+				}
+			}
+		}
+	}
+	
+	private boolean checkBitfield() {
+		BitSet hostBF=syncinfo.getBitfield();
+		BitSet neighborBF=neighbor.getBitfield();
+		hostBF.and(neighborBF);
+		if(hostBF.equals(neighborBF)) return false;
+		return true;
+	}
+	
 
-//           } catch (IOException ioException) {
-//                ioException.printStackTrace();
-
-//	       } catch (ClassNotFoundException e) {
-//               System.err.println("Data received in unknown format!");
-           }
-
-           if (messagereceived instanceof HandShake){
-               if(!handshakeCheck((HandShake)messagereceived, neighbor)){
-                   break;
-               }
-	           if (!handshakefirst){
-	               creatnewhandshake();
-                   System.out.println(hostID+" send handshake to "+neighborID);
-                   
-                   int type=5;
-                   byte[] payload=bits2byte(syncinfo.getBitfield());
-                   int length=payload.length;
-                   ActualMessage bitfield= new ActualMessage(length, type, payload);
-                   sendMsg(bitfield);
-                   
-               }
-               
-           }
-           
-           if(messagereceived instanceof ActualMessage){
-	           ActualMessage msg=(ActualMessage)messagereceived;
-	           
-	           if (msg.getType()==2){
-	               syncinfo.updateInterested(neighborIndex, true);
-                   writetolog();
-	           }
-	           
-	           if(msg.getType()==3){
-	               syncinfo.updateInterested(neighborIndex,false);
-	               
-               }
-               
-               
-           }
-           
-           
-
-
-        }
+	private boolean handshakeCheck(HandShake hs) {
+		// TODO Auto-generated method stub
 
 	}
 
-	public void stopRunning() {
-		running = false;
-	}
-
+	private void creatnewhandshake() {
+		// TODO Auto-generated method stub
 
 	}
 
-	public void stopRunning() {
-		running = false;
+	private int choosePiece() {
+
+	}
+
+	private byte[] int2byte(int i) {
+
+	}
+
+	private int byte2int(byte[] b) {
+
+	}
+
+	private byte[] bits2byte(BitSet bs) {
+
+	}
+	
+	private BitSet byte2bits(byte[] b) {
+		
+	}
+
+	private void sendMsg(ActualMessage msg) {
+
+	}
+
+	private void writelog(String s) {
+
 	}
 
 }
